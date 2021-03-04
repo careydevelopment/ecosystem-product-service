bdc8688b3d742e677374175a5407937d3aba8e98
careydevelopment <mrbrianmcarey@gmail.com>
2021-03-04 16:29:00 -0500

    Support for getting product by ID

--- src/main/java/com/careydevelopment/ecosystem/product/controller/ProductController.java
+++ src/main/java/com/careydevelopment/ecosystem/product/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.careydevelopment.ecosystem.product.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +34,22 @@ public class ProductController {
         List<Product> products = productRepository.findAllByOrderByNameAsc();
         return ResponseEntity.ok(products);
     }
+    
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> fetchProductById(@PathVariable String id) {
+        LOG.debug("Fetching product " + id);
+       
+        Optional<Product> productOpt = productRepository.findById(id);
+        if (productOpt.isPresent()) {
+            Product product = productOpt.get();
+            
+            LOG.debug("Returning product " + product);
+            
+            return ResponseEntity.status(HttpStatus.OK).body(product);   
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Could not find product " + id);
+        }        
+    }
+
 }
